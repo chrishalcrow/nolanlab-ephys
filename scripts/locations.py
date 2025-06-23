b90bedec224c8f93bff05c7130652f78a1e65ddf
chrishalcrow <chrishalcrow@gmail.com>
2025-06-23 17:11:07 +0100

    add sa stagein

--- scripts/locations.py
+++ scripts/locations.py
@@ -13,6 +13,15 @@ parser.add_argument('sessions')
 data_folder = Path("/exports/eddie/scratch/chalcrow/harry/data/")
 deriv_folder = Path("/exports/eddie/scratch/chalcrow/harry/derivatives/")
 
+DS_extensions_path = Path(f"Chris/Cohort12/derivatives/M{mouse}/D{day}/full/kilosort4/kilosort4_sa/extensions/")
+extensions_path = Path(f"M{mouse}/D{day}/full/kilosort4/kilosort4_sa/extensions/")
+locations_path = extensions_path / "spike_locations"
+
+sa_path = [f"Chris/Cohort12/derivatives/M{mouse}/D{day}/full/kilosort4/kilosort4_sa"]
+
+destination = Path(deriv_folder) / f"M{mouse}/D{day}/full/kilosort4"
+destination.mkdir(exist_ok=True, parents=True)
+
 mouse = int(parser.parse_args().mouse)
 day = int(parser.parse_args().day)
 sessions_string = parser.parse_args().sessions
@@ -23,11 +32,11 @@ recording_paths = filepath_from_mouse_day_sessions(mouse, day, sessions)
 active_projects_path = eddie_active_projects
 
 stagein_script_name = f"M{mouse}D{day}_in"
-make_and_run_script(active_projects_path, recording_paths, data_folder, stagein_script_name)
-make_and_run_locations_script(mouse, day, wait_for=f" -hold_jid {stagein_script_name}")
+stagein_sa_script_name = f"M{mouse}D{day}_sain"
 
-DS_extensions_path = Path(f"Chris/Cohort12/derivatives/M{mouse}/D{day}/full/kilosort4/kilosort4_sa/extensions/")
-extensions_path = Path(f"M{mouse}/D{day}/full/kilosort4/kilosort4_sa/extensions/")
-locations_path = extensions_path / "spike_locations"
+make_and_run_script(active_projects_path, sa_path, destination, stagein_sa_script_name)
+make_and_run_script(active_projects_path, recording_paths, data_folder, stagein_script_name)
+
+make_and_run_locations_script(mouse, day, wait_for=f" -hold_jid {stagein_script_name}")
 
 make_and_run_stageout(Path(deriv_folder) / locations_path, active_projects_path, DS_extensions_path, wait_for=f" -hold_jid M{mouse}D{day}loc")
