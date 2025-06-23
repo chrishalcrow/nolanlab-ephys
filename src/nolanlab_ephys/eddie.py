c1fa9c0dfac75ff5569f4cb8458fcb1798ea6ed5
chrishalcrow <chrishalcrow@gmail.com>
2025-06-23 15:53:40 +0100

    add make_and_run_locations_script

--- src/nolanlab_ephys/eddie.py
+++ src/nolanlab_ephys/eddie.py
@@ -18,7 +18,7 @@ def make_and_run_script(active_projects_path, recording_paths, destination_folde
     for recording_path in recording_paths:
         script_text += f"cp -rn {active_projects_path + recording_path} {destination_folder}\n"
 
-    filename = 'stagein_{date:%Y-%m-%d_%H:%M:%S}_{rando}.txt'.format( date=datetime.now(), rando= random.randint(100))  
+    filename = 'stagein_{date:%Y-%m-%d_%H:%M:%S}_{rando}.sh'.format( date=datetime.now(), rando= random.randint(100))  
 
     with open(filename, "w") as f:
         f.write(script_text)
@@ -33,3 +33,26 @@ def filepath_from_mouse_day_sessions(mouse, day, sessions, path_to_all_filepaths
 
     return mouseday_filepaths
 
+
+
+def make_and_run_locations_script(mouse, day):
+
+    script_text = f"""# Grid Engine options start with a #$
+#$ -N {mouse}_{day}_loc
+#$ -pe sharedmem 4 -l rl9=true,h_vmem=30G,h_rt=48:00:00
+#$ -cwd
+
+# Hard runtime limit
+#$ -l h_rt=02:00:00 
+
+cd /exports/eddie/scratch/chalcrow/harry/fromgit/nolanlab-ephys
+
+uv run scripts/compute_locations_and_pcs.py {mouse} {day}
+"""
+
+    filename = f'locations_{mouse}_{day}.sh'
+
+    with open(filename, "w") as f:
+        f.write(script_text)
+
+    subprocess.run(['qsub', filename])
