c5300dc9511782c78b3f44a109b88f9acfd054df
chrishalcrow <chrishalcrow@gmail.com>
2025-06-23 14:52:34 +0100

    stagein files scripts

---
+++ scripts/stagein_files.py
@@ -0,0 +1,19 @@
+from nolanlab_ephys.eddie import filepath_from_mouse_day_sessions, make_script
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+
+parser.add_argument('mouse')
+parser.add_argument('day')
+parser.add_argument('sessions')
+
+mouse = int(parser.parse_args().mouse)
+day = int(parser.parse_args().day)
+sessions_string = parser.parse_args().sessions
+sessions = sessions_string.split(',')
+
+recording_paths = filepath_from_mouse_day_sessions(mouse, day, sessions)
+
+active_projects_path = "/run/user/1000/gvfs/smb-share:server=cmvm.datastore.ed.ac.uk,share=cmvm/sbms/groups/CDBS_SIDB_storage/NolanLab/ActiveProjects/"
+
+make_script(active_projects_path, recording_paths, "here!")
---
+++ src/nolanlab_ephys/eddie.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+import numpy.random as random
+import pandas as pd
+
+def make_and_run_script(active_projects_path, recording_paths, destination_folder):
+
+    script_text = """# Grid Engine options start with a #$
+#$ -N stageout
+#$ -cwd
+#$ -q staging
+
+# Hard runtime limit
+#$ -l h_rt=00:30:00 
+
+"""
+
+    for recording_path in recording_paths:
+        script_text += f"cp -rn {active_projects_path + recording_path} {destination_folder}\n"
+
+    filename = 'stagein_{date:%Y-%m-%d_%H:%M:%S}_{rando}.txt'.format( date=datetime.now(), rando= random.randint(100))  
+
+    with open(filename, "w") as f:
+        f.write(script_text)
+
+    
+
+
+
+def filepath_from_mouse_day_sessions(mouse, day, sessions, path_to_all_filepaths="all_filepaths.csv"):
+
+    all_filepaths = pd.read_csv(path_to_all_filepaths)
+
+    mouseday_filepaths = [all_filepaths.query(f'mouse == {mouse} & day == {day} & session == "{session}"')['filepath'].values[0] for session in sessions]
+
+    return mouseday_filepaths
+
