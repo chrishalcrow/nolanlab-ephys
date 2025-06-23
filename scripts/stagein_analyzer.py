13a8f5ff258d3b9dc44db6140d61b21112e167cf
chrishalcrow <chrishalcrow@gmail.com>
2025-06-23 15:32:08 +0100

    stagein analyzer

--- scripts/stagein_analyzer.py
+++ scripts/stagein_analyzer.py
@@ -1,20 +1,23 @@
 from nolanlab_ephys.eddie import make_and_run_script
 from argparse import ArgumentParser
 from nolanlab_ephys.common_paths import chris_linux_active_projects
-
+from pathlib import Path
 
 parser = ArgumentParser()
 
 parser.add_argument('mouse')
 parser.add_argument('day')
-parser.add_argument('destination')
+parser.add_argument('derivatives')
 
 mouse = int(parser.parse_args().mouse)
 day = int(parser.parse_args().day)
-destination = parser.parse_args().destination
+derivatives = parser.parse_args().derivatives
 
 sa_path = [f"Chris/Cohort12/derivatives/M{mouse}/D{day}/full/kilosort4/kilosort4_sa"]
 
 active_projects_path = chris_linux_active_projects
+
+destination = Path(derivatives / f"M{mouse}/D{day}/full/kilosort4/kilosort4_sa")
+destination.mkdir(exist_ok=True, parents=True)
 
 make_and_run_script(active_projects_path, sa_path, destination)
