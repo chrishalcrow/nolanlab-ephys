5a0cafa74da6ac336fe23c56e37a9078910bbfa9
chrishalcrow <chrishalcrow@gmail.com>
2025-06-23 16:56:27 +0100

    add locations.py

---
+++ scripts/locations.py
@@ -0,0 +1,34 @@
+from nolanlab_ephys.eddie import filepath_from_mouse_day_sessions, make_and_run_script, make_and_run_locations_script, make_and_run_stageout
+from argparse import ArgumentParser
+from nolanlab_ephys.common_paths import eddie_active_projects
+from pathlib import Path
+
+parser = ArgumentParser()
+
+parser.add_argument('mouse')
+parser.add_argument('day')
+parser.add_argument('sessions')
+
+
+data_folder = Path("/exports/eddie/scratch/chalcrow/harry/data/")
+deriv_folder = Path("/exports/eddie/scratch/chalcrow/harry/derivatives/")
+
+mouse = int(parser.parse_args().mouse)
+day = int(parser.parse_args().day)
+sessions_string = parser.parse_args().sessions
+sessions = sessions_string.split(',')
+destination = parser.parse_args().destination
+
+recording_paths = filepath_from_mouse_day_sessions(mouse, day, sessions)
+
+active_projects_path = eddie_active_projects
+
+stagein_script_name = f"M{mouse}D{day}_in"
+make_and_run_script(active_projects_path, recording_paths, data_folder, stagein_script_name)
+make_and_run_locations_script(mouse, day, wait_for=f",-hold_jid {stagein_script_name}")
+
+DS_extensions_path = Path(f"Chris/Cohort12/derivatives/M{mouse}/D{day}/full/kilosort4/kilosort4_sa/extensions/")
+extensions_path = Path(f"M{mouse}/D{day}/full/kilosort4/kilosort4_sa/extensions/")
+locations_path = extensions_path / "spike_locations"
+
+make_and_run_stageout(Path(deriv_folder) / locations_path, active_projects_path, DS_extensions_path, wait_for=f",-hold_jid M{mouse}D{day}loc")
