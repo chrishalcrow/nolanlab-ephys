3b88b03362a109794dc2042cc74f45ca7a0e8fd9
chrishalcrow <chrishalcrow@gmail.com>
2025-06-23 14:56:57 +0100

    add destination

--- scripts/stagein_files.py
+++ scripts/stagein_files.py
@@ -1,4 +1,4 @@
-from nolanlab_ephys.eddie import filepath_from_mouse_day_sessions, make_script
+from nolanlab_ephys.eddie import filepath_from_mouse_day_sessions, make_and_run_script
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
@@ -6,14 +6,16 @@ parser = ArgumentParser()
 parser.add_argument('mouse')
 parser.add_argument('day')
 parser.add_argument('sessions')
+parser.add_argument('destination')
 
 mouse = int(parser.parse_args().mouse)
 day = int(parser.parse_args().day)
 sessions_string = parser.parse_args().sessions
 sessions = sessions_string.split(',')
+destination = parser.parse_args().destination
 
 recording_paths = filepath_from_mouse_day_sessions(mouse, day, sessions)
 
 active_projects_path = "/run/user/1000/gvfs/smb-share:server=cmvm.datastore.ed.ac.uk,share=cmvm/sbms/groups/CDBS_SIDB_storage/NolanLab/ActiveProjects/"
 
-make_script(active_projects_path, recording_paths, "here!")
+make_and_run_script(active_projects_path, recording_paths, destination)
