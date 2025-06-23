a10047ad3c341a9ac37d3dedb013330e5461faa2
chrishalcrow <chrishalcrow@gmail.com>
2025-06-23 16:30:45 +0100

    stageout locations

--- src/nolanlab_ephys/eddie.py
+++ src/nolanlab_ephys/eddie.py
@@ -25,6 +25,28 @@ def make_and_run_script(active_projects_path, recording_paths, destination_folde
 
     subprocess.run(['qsub', filename])
 
+
+def make_and_run_stageout(source_folder, active_projects_path, destination_folder):
+
+    script_text = """# Grid Engine options start with a #$
+#$ -N stageout
+#$ -cwd
+#$ -q staging
+
+# Hard runtime limit
+#$ -l h_rt=00:30:00 
+
+"""
+
+    script_text += f"cp -rn {source_folder} {active_projects_path / destination_folder}\n"
+
+    filename = 'stageout_{date:%Y-%m-%d_%H:%M:%S}_{rando}.sh'.format( date=datetime.now(), rando= random.randint(100))  
+
+    with open(filename, "w") as f:
+        f.write(script_text)
+
+    subprocess.run(['qsub', filename])
+
 def filepath_from_mouse_day_sessions(mouse, day, sessions, path_to_all_filepaths="all_filepaths.csv"):
 
     all_filepaths = pd.read_csv(path_to_all_filepaths)
@@ -32,6 +54,8 @@ def filepath_from_mouse_day_sessions(mouse, day, sessions, path_to_all_filepaths
     mouseday_filepaths = [all_filepaths.query(f'mouse == {mouse} & day == {day} & session == "{session}"')['filepath'].values[0] for session in sessions]
 
     return mouseday_filepaths
+
+
 
 
 
