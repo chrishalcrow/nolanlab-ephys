d5e814c10389683584b9aba6b6c7142cab6fb6fb
chrishalcrow <chrishalcrow@gmail.com>
2025-07-07 16:07:24 +0100

    add sort on eddie

---
+++ scripts/sort_on_eddie.py
@@ -0,0 +1,8 @@
+from eddie_helper.make_scripts import run_python_script
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument('python_arg')
+python_arg = parser.parse_args().python_arg
+
+run_python_script(python_arg, "chalcrow", cores=8, email="chalcrow@ed.ac.uk", staging=False)
