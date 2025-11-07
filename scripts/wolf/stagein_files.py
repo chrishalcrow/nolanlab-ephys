c1fcc7a7bcbeb3c26bdca95443a24646bda79e14
chrishalcrow <chrishalcrow@gmail.com>
2025-11-07 09:27:19 +0000

    update stagein

--- scripts/wolf/stagein_files.py
+++ scripts/wolf/stagein_files.py
@@ -1,6 +1,7 @@
 from nolanlab_ephys.eddie import filepath_from_mouse_day_sessions, make_and_run_script
 from argparse import ArgumentParser
 from nolanlab_ephys.common_paths import eddie_active_projects
+from nolanlab_ephys.utils import get_recording_folders
 
 parser = ArgumentParser()
 
@@ -15,12 +16,12 @@ sessions_string = parser.parse_args().sessions
 sessions = sessions_string.split(',')
 destination = parser.parse_args().destination
 
-recording_paths = filepath_from_mouse_day_sessions(mouse, day, sessions)
-
 active_projects_path = eddie_active_projects
+data_path = active_projects_path / "Harry/EphysNeuropixelData/"
+
+recording_paths = get_recording_folders(data_path, mouse, day)
+
+print(f"Getting recordings {recording_paths}.")
 
 stagein_script_name = f"M{mouse}D{day}_in"
 script_name = make_and_run_script(active_projects_path, recording_paths, destination, stagein_script_name)
-
-
-
