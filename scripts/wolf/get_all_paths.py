8b39df346b56732ddb3ed9c9717347de4f724078
chrishalcrow <chrishalcrow@gmail.com>
2025-11-17 22:38:22 +0000

    add making folders

--- scripts/wolf/get_all_paths.py
+++ scripts/wolf/get_all_paths.py
@@ -27,5 +27,22 @@ for recording_folder in recording_folders:
     
     data.append( [mouse, day, session, path_from_active] )
 
-df = pd.DataFrame(data, columns=["mouse", "day", "session", "filepath"])
-df.to_csv("scripts/wolf/wolf_filepaths.csv", index=False)
+all_filepaths = pd.DataFrame(data, columns=["mouse", "day", "session", "filepath"])
+all_filepaths.to_csv("scripts/wolf/wolf_filepaths.csv", index=False)
+
+derivatives_folder = Path("/Volumes/cmvm/sbms/groups/CDBS_SIDB_storage/NolanLab/ActiveProjects/Wolf/MMNAV/derivatives")
+
+for _, row in all_filepaths.iterrows():
+    
+    mouse = row['mouse']
+    day = row['day']
+    session = row['session']
+    filepath = row['filepath']
+
+    print(f'mouse {mouse} day {day} session {session}')
+
+    mousedaysession_folder = derivatives_folder / f'M{mouse:02d}' / f'D{day:02d}' / f'{session}'
+
+    mousedaysession_folder.parent.parent.mkdir(exist_ok=True)
+    mousedaysession_folder.parent.mkdir(exist_ok=True)
+    mousedaysession_folder.mkdir(exist_ok=True)
