09f1bd5c22db2c65e2be3bc054dedb0bd61c5545
chrishalcrow <chrishalcrow@gmail.com>
2026-03-05 14:56:27 +0000

    make analyzers

--- scripts/harry/split_analyzers_into_sessions_comp.py
+++ scripts/harry/split_analyzers_into_sessions_comp.py
@@ -1,6 +1,7 @@
 import spikeinterface.full as si
 
-from nolanlab_ephys.utils import get_chrono_concat_recording
+import pandas as pd
+from nolanlab_ephys.si_protocols import generic_postprocessing
 from pathlib import Path
 import numpy as np
 from argparse import ArgumentParser
@@ -16,10 +17,49 @@ parser.add_argument('day')
 mouse = int(parser.parse_args().mouse)
 day = int(parser.parse_args().day)
 
+all_rec_samples = pd.read_csv("scripts/harry/resources/all_rec_samples_of_vr_of.csv")
+samples = all_rec_samples.query(f'mouse == {mouse} & day == {day}')
+
+of1_samples, vr_samples, of2_samples = samples[['of1','vr','of2']].values[0]
+
+print(of1_samples, vr_samples, of2_samples)
+
 si.set_global_job_kwargs(n_jobs=8)
 
 analyzer_path = deriv_folder / f"M{mouse}/D{day}/full/kilosort4/sub-{mouse}_ses-{day}_srt-kilosort4_full_analyzer.zarr"
 analyzer = si.load_sorting_analyzer(analyzer_path)
+
+sorting = analyzer.sorting
+recording = analyzer.recording
+
+of1_sorting = sorting.frame_slice(start_frame=0, end_frame=of1_samples)
+of1_recording = recording.frame_slice(start_frame=0, end_frame=of1_samples)
+
+vr_sorting = sorting.frame_slice(start_frame=of1_samples, end_frame=of1_samples+vr_samples)
+vr_recording = recording.frame_slice(start_frame=of1_samples, end_frame=of1_samples+vr_samples)
+
+of2_sorting = sorting.frame_slice(start_frame=of1_samples+vr_samples, end_frame=None)
+of2_recording = recording.frame_slice(start_frame=of1_samples+vr_samples, end_frame=None)
+
+sortings = [of1_sorting, vr_sorting, of2_sorting]
+recordings = [of1_recording, vr_recording, of2_recording]
+typs = ['OF1', 'VR', 'OF2']
+
+for recording, sortings, typ in zip(recordings, sortings, typs):
+
+    # we do all our syncing assuming that t=0 is at the start of the ephys data
+    recording._recording_segments[0].t_start = 0
+
+    analyzer = si.create_sorting_analyzer(
+        recording=recording,
+        sorting=sorting, 
+        folder = deriv_folder / f"M{mouse:02d}/D{day:02d}/{typ}/kilosort4/sub-{mouse:02d}_ses-{day:02d}_typ-{typ}_srt-kilosort4_analyzer",
+        format = "zarr",
+        peak_sign = "both",
+        radius_um = 70,
+    )
+
+    analyzer.compute(generic_postprocessing)
 
 #recording = get_chrono_concat_recording(data_folder=data_folder, mouse =mouse, day=day)
 #recording = si.aggregate_channels(recording.split_by('group'))
