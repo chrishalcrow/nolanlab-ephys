fb3b978c1b703bd9e5e6d6ec4103faef96d3d089
chrishalcrow <chrishalcrow@gmail.com>
2026-05-26 08:44:52 +0100

    Add quality control plots for Wolf

--- M5D4MMin2026-04-30_11-04-56.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#$ -cwd
-#$ -q staging
-#$ -l h_rt=00:29:59 -N M5D4MMin
-cp -rn /exports/cmvm/datastore/sbms/groups/CDBS_SIDB_storage/NolanLab/ActiveProjects/Wolf/MMNAV/raw/VR/M05_D04_2025-11-16_14-46-40_MMNAV1 /exports/eddie/scratch/chalcrow/wolf/raw/VR/M05_D04_2025-11-16_14-46-40_MMNAV1
