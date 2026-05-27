51f98065265c525f5b9762535bc60840d4e3f0e4
chrishalcrow <chrishalcrow@gmail.com>
2026-05-27 08:37:38 +0100

    add quality control to wolf's default run

--- scripts/wolf/sort_on_eddie.py
+++ scripts/wolf/sort_on_eddie.py
@@ -72,11 +72,15 @@ stageout_dict[deriv_folder / f"M{mouse:02d}/D{day:02d}/M{mouse:02d}_D{day:02d}_p
 
 stagein_job_name = f"M{mouse}D{day}{sessions[0][:2]}in" 
 run_python_name = f"M{mouse}D{day}{sessions[0][:2]}run"
+quality_name = f"M{mouse}D{day}quality"
 stageout_job_name = f"M{mouse}D{day}{sessions[0][:2]}out" 
 
 uv_directory = os.getcwd()
 python_arg = f"scripts/wolf/sort_on_comp.py {mouse} {day} {sessions_string} {protocol} --data_folder={data_folder} --deriv_folder={deriv_folder}"
+quality_arg = f"scripts/wolf/quality_controls.py {mouse} {day} {sessions_string} {protocol} --data_folder={data_folder} --deriv_folder={deriv_folder}"
 
 run_stage_script(stagein_dict, job_name=stagein_job_name)
 run_python_script(uv_directory, python_arg, cores=8, email=email, staging=False, hold_jid=stagein_job_name, job_name=run_python_name)
+# Do quality control
+run_python_script(uv_directory, python_arg, cores=8, email=email, staging=False, hold_jid=stagein_job_name, job_name=quality_name)
 run_stage_script(stageout_dict, job_name=stageout_job_name, hold_jid=run_python_name)
