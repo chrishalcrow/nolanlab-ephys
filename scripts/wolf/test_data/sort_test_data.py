2ba01c4e0574f9932c246fd5fb724e5804e79e4f
chrishalcrow <chrishalcrow@gmail.com>
2026-06-03 09:16:38 +0100

    Add Wolf test data

---
+++ scripts/wolf/test_data/sort_test_data.py
@@ -0,0 +1,43 @@
+
+from argparse import ArgumentParser
+from pathlib import Path
+from nolanlab_ephys.lab_utils import get_recording_folders, chronologize_paths
+from nolanlab_ephys.sort import do_sorting_pipeline_concat
+from .test_data_eddie import recording_paths
+import spikeinterface.full as si
+
+def main():
+
+    parser = ArgumentParser()
+
+    parser.add_argument('--data_folder', default=None)
+    parser.add_argument('--deriv_folder', default=None)
+
+    deriv_folder = parser.parse_args().deriv_folder
+    data_folder = parser.parse_args().data_folder
+    
+    print(f"\nWill sort the following recordings:")
+    for recording_path in recording_paths:
+        print(f"  - {recording_path}")
+
+    recordings = [si.read_openephys(recording_path) for recording_path in recording_paths]
+
+    protocol = 'lupinA'
+
+    analyzer_paths = [
+        deriv_folder / recording_path.name for recording_path in recording_paths
+    ]
+
+    for recording, analyzer_path in zip(recordings, analyzer_paths):
+    
+        do_sorting_pipeline_concat(
+            recording,
+            analyzer_path,
+            protocol,
+            sorting_output_folder=f"sorting_output_{analyzer_path.name}",
+            n_jobs=8,
+        )
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/wolf/test_data/test_data_eddie.py
@@ -0,0 +1,40 @@
+from eddie_helper.make_scripts import run_python_script, run_stage_script
+from argparse import ArgumentParser
+from pathlib import Path
+from common_paths import  eddie_active_projects, eddie_wolf_data_folder, eddie_wolf_deriv_folder
+import os
+import pandas as pd
+    
+parser = ArgumentParser()
+
+data_folder = Path()
+deriv_folder = Path()
+
+active_projects_path = eddie_active_projects
+test_folder = active_projects_path / "Wolf/mmnav/data/test"
+
+folder_names = [
+    "M3_2025-11-11_15-14-58_S1_test",
+]
+
+recording_paths = [
+    test_folder / folder_name for folder_name in folder_names
+]
+
+stagein_dict = {}
+for recording_path in recording_paths:
+    recording_folder_name = Path(recording_path).name
+    session_type_folder = data_folder / "VR"
+    stagein_dict[f"{active_projects_path / recording_path}"] = session_type_folder / recording_folder_name
+    session_type_folder.mkdir(exist_ok=True)
+
+stagein_job_name = f"test_in" 
+run_python_name = f"test_run"
+
+uv_directory = os.getcwd()
+python_arg = f"scripts/wolf/test_data/sort_test_data.py --data_folder={data_folder} --deriv_folder={deriv_folder}"
+
+email = "chalcrow@ed.ac.uk"
+
+run_stage_script(stagein_dict, job_name=stagein_job_name)
+run_python_script(uv_directory, python_arg, cores=8, email=email, staging=False, hold_jid=stagein_job_name, job_name=run_python_name)
